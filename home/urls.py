dc799dd056e15fc74f8848fe7e37a748ceaa41a7
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-14 04:10:15 +0800

    view for urls

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
+    path('book/', views.book),
+    path('comfirm/', views.comfirm),
+    path('forgrt/', views.forgrt),
+    path('home/', views.home),
     path('login/', views.login),
+    path('participants/', views.participants),
+    path('records/', views.records),
+    path('register/', views.register),
+    path('report/', views.report),
+    path('search/', views.search),
     path('accounts/login/', views.user_login),
 ]
