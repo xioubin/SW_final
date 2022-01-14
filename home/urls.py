2bf65db4c73624f7890fb0c5f80d834dfa28b614
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-15 02:26:23 +0800

    modify url with name

--- home/urls.py
+++ home/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('book/', views.book),
-    path('comfirm/', views.comfirm),
-    path('forgrt/', views.forgrt),
-    path('home/', views.home),
-    path('login/', views.login),
-    path('participants/', views.participants),
-    path('records/', views.records),
-    path('register/', views.register),
-    path('report/', views.report),
-    path('search/', views.search),
+    path('', views.home, name='home'),
+    path('book/', views.book, name='book'),
+    path('comfirm/', views.comfirm, name='comfirm'),
+    path('forgrt/', views.forgrt, name='forgrt'),
+    path('home/', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('participants/', views.participants, name='participants'),
+    path('records/', views.records, name='records'),
+    path('register/', views.register, name='register'),
+    path('report/', views.report, name='report'),
+    path('search/', views.search, name='search'),
     path('accounts/login/', views.user_login),
 ]
