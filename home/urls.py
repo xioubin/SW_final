35ea18b36ad94c6cdb96797fa9d6c82a9946e778
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-15 21:54:52 +0800

    delete redirect home

--- home/urls.py
+++ home/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('home/', views.index, name='index'),
+
     path('book/', views.book, name='book'),
     path('comfirm/', views.comfirm, name='comfirm'),
     path('forget/', views.forget, name='forget'),
-    path('home/', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('participants/', views.participants, name='participants'),
     path('records/', views.records, name='records'),
