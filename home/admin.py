9a221e1b6d934853060d76557e7e0a6f173162a3
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-19 02:37:40 +0800

    update admin

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 
-from .models import User_Info, Reservation
+from .models import User_Info, Reservation, ErrorReport
 
 admin.site.register(User_Info)
 admin.site.register(Reservation)
+admin.site.register(ErrorReport)
+
 # Register your models here.
