12ab9570fb8779bfba985f6f207f5cfd927cfa1e
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-18 17:51:18 +0800

    registre

--- home/form.py
+++ home/form.py
@@ -3,9 +3,9 @@ from .models import User_Info
 
 
 class RegisterForm(forms.ModelForm):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=100)
+    # username = forms.CharField(max_length=100)
+    # password = forms.CharField(max_length=100)
+    # email = forms.EmailField(max_length=100)
 
     class Meta:
         model = User_Info
