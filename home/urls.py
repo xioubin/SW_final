8942f6f5c1983307e8461fffc19babfe7fd7db00
Mandy <mandy900619@gmail.com>
2022-01-20 01:54:15 +0800

    main\

--- home/urls.py
+++ home/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('records/', views.records, name='records'),
     path('register/', views.register, name='register'),
     path('report/', views.report, name='report'),
+
     # path('accounts/login/', views.user_login),
 ]
