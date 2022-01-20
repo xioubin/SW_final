91256ae327bcecaf3ed5fab5a8ec255985f43420
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-01-21 05:51:48 +0800

    login require

--- home/urls.py
+++ home/urls.py
@@ -1,21 +1,22 @@
 from django.urls import path
+from django .contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/', views.book, name='book'),
-    path('book/$', views.book, name='book'),
-    path('modify/', views.modify, name='modify'),
-    path('modify/$', views.modify, name='modify'),
+    path('book/', login_required(views.book), name='book'),
+    path('book/$', login_required(views.book), name='book'),
+    path('modify/', login_required(views.modify), name='modify'),
+    path('modify/$', login_required(views.modify), name='modify'),
+    path('records/', login_required(views.records), name='records'),
 
 
-    path('comfirm/', views.comfirm, name='comfirm'),
+    # path('comfirm/', views.comfirm, name='comfirm'),
     path('forget/', views.forget, name='forget'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 
-    path('participants/', views.participants, name='participants'),
-    path('records/', views.records, name='records'),
+    # path('participants/', views.participants, name='participants'),
     path('register/', views.register, name='register'),
     path('report/', views.report, name='report'),
 
