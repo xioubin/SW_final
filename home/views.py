1e1636122511f2e1455bcb2d90fbac49c31e87d2
Yubin <b10830038@gapps.ntust.edu.tw>
2022-01-21 15:35:52 +0800

    0

--- home/views.py
+++ home/views.py
@@ -69,8 +69,7 @@ def book(request):
             text = 'You successfully reservated a meeting room. Please remember your meeting time.\nYour have reservated: ' + request.POST.get('date') + ' at ' + Reservation.TIME_CHOICES[int(request.POST.get('time'))][1] + '\n'
             mail = request.user
             send_mail('借用成功通知', text, None, [mail])
-            print(form.cleaned_data)
-            send_mail('會議邀請通知', '你他媽的給我來參加會議喔', None, form.cleaned_data.get('invitees'))
+            send_mail('會議邀請通知', 'You have been invited to a meeting at ' + Reservation.TIME_CHOICES[int(request.POST.get('time'))][1] + ' Please check on your calander.', None, form.cleaned_data.get('invitees'))
             form.save()
             # auth.login(request, user)
             return redirect('/home/')
@@ -93,7 +92,7 @@ def modify(request):
         form = ModifyForm(request.POST, instance=reservation)
         if form.is_valid():
             print(form.cleaned_data)
-            send_mail('更改會議成功', 'Your reservation has been changed. Please check on your calander.', None, form.cleaned_data.get('invitees'))
+            send_mail('更改會議成功', 'Your reservation has been changed to '+ Reservation.TIME_CHOICES[int(request.POST.get('time'))][1] +'. Please check on your calander.', None, form.cleaned_data.get('invitees'))
             form.save()
             return redirect('/home/records/')
     print(auto_increment_id)
